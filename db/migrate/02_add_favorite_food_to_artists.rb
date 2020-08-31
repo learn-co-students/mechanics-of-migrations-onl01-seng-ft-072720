62a3c84a30500b18230012d0445951bdae374727
Evan Pavone <evanrpavone@gmail.com>
2020-08-31 14:37:59 +0000

    Done.

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -2,4 +2,5 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
     def change
       add_column :artists, :favorite_food, :string
     end
-  end 
+  end
+#edit: IGNORE would not submit to learn.co this is just to be able to submit
